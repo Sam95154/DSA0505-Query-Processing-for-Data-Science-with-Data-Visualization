eef810f9fd8455a9447380eafad8723159241e15
shaik sameer basha <113328699+Sam95154@users.noreply.github.com>
2024-02-23 08:42:36 +0530

    Create 22. line plot.py

---
+++ 22. line plot.py
@@ -0,0 +1,8 @@
+import matplotlib.pyplot as plt
+x = [1, 2, 3, 4, 5]
+y = [2, 4, 1, 3, 6]
+plt.plot(x, y)
+plt.xlabel("X Axis Label")
+plt.ylabel("Y Axis Label")
+plt.title("Line Plot Example")
+plt.show()
