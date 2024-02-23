c3688f980a8e0672de8fb6368f0516f97feb52c2
shaik sameer basha <113328699+Sam95154@users.noreply.github.com>
2024-02-23 08:26:05 +0530

    Create 14. find and replace missing value.py

---
+++ 14. find and replace missing value.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+data = {
+    'A': [1, 2, np.nan, 4, 5],
+    'B': [np.nan, 2, 3, 4, np.nan],
+    'C': [1, 2, 3, np.nan, 5]
+}
+df = pd.DataFrame(data)
+value_to_replace = -1
+df_filled = df.fillna(value_to_replace)
+print(df_filled)
