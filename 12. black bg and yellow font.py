0c24f7e4003baf66812af1d4e3f07f8cf5487fc3
shaik sameer basha <113328699+Sam95154@users.noreply.github.com>
2024-02-23 08:24:36 +0530

    Create 12. black bg and yellow font.py

---
+++ 12. black bg and yellow font.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+np.random.seed(24)
+df = pd.DataFrame({'A': np.linspace(1, 10, 10)})
+df = pd.concat([df, pd.DataFrame(np.random.randn(10, 4), columns=list('BCDE'))],
+               axis=1)
+df.iloc[0, 2] = np.nan
+df.iloc[3, 3] = np.nan
+df.iloc[4, 1] = np.nan
+df.iloc[9, 4] = np.nan
+print("Original array:")
+print(df)
+print("\nBackground:black - fontcolor:yelow")
+df.style.set_properties(**{'background-color': 'black',
+                           'color': 'yellow'})
