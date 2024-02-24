03b12adc4490671459402ddae8fcedded2ec14be
shaik sameer basha <113328699+Sam95154@users.noreply.github.com>
2024-02-24 08:45:09 +0530

    Create 39.first 3 rows from df.py

---
+++ 39.first 3 rows from df.py
@@ -0,0 +1,10 @@
+import pandas as pd
+data={'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19]
+}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df=pd.DataFrame(data,index=labels)
+first=df.head(3)
+print("First 3 Rows of given Dataframe :" ,first)
