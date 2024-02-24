f06abc8e46b362da3ddbbf84879868061ddb7b94
shaik sameer basha <113328699+Sam95154@users.noreply.github.com>
2024-02-24 08:45:38 +0530

    Create 40.select cols from df.py

---
+++ 40.select cols from df.py
@@ -0,0 +1,10 @@
+import pandas as pd
+import numpy as np
+data={'attempts': [1, 3, 2, 3, 2, 3, 1, 1, 2, 1],
+'name': ['Anastasia', 'Dima', 'Katherine', 'James', 'Emily', 'Michael', 'Matthew', 'Laura', 'Kevin', 'Jonas'],
+'qualify': ['yes', 'no', 'yes', 'no', 'no', 'yes', 'yes', 'no', 'no', 'yes'],
+'score': [12.5, 9, 16.5, np.nan, 9, 20, 14.5, np.nan, 8, 19]
+}
+labels = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j']
+df=pd.DataFrame(data,index=labels)
+print(df[['name','score']])
